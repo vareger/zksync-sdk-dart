ecf7849a3ae8f32723eacd17c6efdaae740ad439
MaximFischuk <mfischuk@vareger.com>
2021-03-08 06:15:19 +0200

    Add default value for time range

--- lib/src/client/zksync/time_range.dart
+++ lib/src/client/zksync/time_range.dart
@@ -8,6 +8,8 @@ class TimeRange {
       : this._from = DateTime.fromMillisecondsSinceEpoch(from * 1000),
         this._until = DateTime.fromMillisecondsSinceEpoch(until * 1000);
 
+  TimeRange.def() : this.raw(0, 4294967295);
+
   DateTime get validFrom => _from;
   DateTime get validUntil => _until;
 
